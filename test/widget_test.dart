84af588855a9ee9801cc9b752e6de326c6c4652f
Mr_DeBuFF <a.bairamov@innopolis.university>
2024-07-02 04:09:23 +0300

    complete tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,22 +9,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:start_folder_to_zip/main.dart';
+import '../lib/pages/day_page.dart';
+import '../lib/pages/welkome_page.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Widget test', (WidgetTester tester) async {
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    await tester.pumpWidget(const MaterialApp(home: WelkomePage()));
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    expect(find.text("Welcome"), findsOneWidget);
+    expect(find.text("started!"), findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
